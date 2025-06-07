7061a6991e4ac8e530028c45755eb2c8979b603e
Karanjot Singh <113842752+Karanxidhu@users.noreply.github.com>
2025-06-08 02:58:40 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const cors = require('cors');
 const connectToMongo = require('./db');
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 connectToMongo();
 app.use(express.json());
