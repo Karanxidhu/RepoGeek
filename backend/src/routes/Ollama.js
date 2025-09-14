730d6f5b04ebdc502367a4a6a9cb31080252f579
karanxidhu <sidhu.karanjot22@gmail.com>
2025-09-14 23:41:13 +0530

    hehe

--- backend/src/routes/Ollama.js
+++ backend/src/routes/Ollama.js
@@ -3,12 +3,13 @@ const fetchuser = require('../middleware/middleware');
 const router = express.Router();
 const NodeCache = require('node-cache');
 const myCache = new NodeCache();
+require('dotenv').config()
 
 // Import Gemini (Generative AI) client
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 // Initialize Gemini client
-const genAI = new GoogleGenerativeAI("AIzaSyDfnlBGHsbt9qg_T7bUofqs4ZUXu472Efg"); // Set your API key in environment variable
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API); // Set your API key in environment variable
 
 const model = "gemini-2.5-flash"; // Or another Gemini model
 
